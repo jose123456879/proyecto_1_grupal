9b8296278a0949d41bfa402281d7fa331c3c3e36
ricarojuarez <ricardojuarez7>
2016-05-30 14:35:53 -0600

    relialice el prob 7

--- prob_7.py
+++ prob_7.py
@@ -1,11 +1,11 @@
 def prob_7 ():
-	cuatro = []
-	siete = []
+	lista = []
 	for i in range(0,250):
 		cu = 4*i
 		if cu < 1000:
-			cuatro.append(cu)
+			lista.append(cu)
 		si = 7*i
 		if si < 1000:
-			siete.append(si)
-	return (cuatro,siete)
+			lista.append(si)
+	lista.sort()
+	return lista
