e386cbe1a5c2d463ff419fd6235fb7b5b09d8659
ricardojuarez <ricardojuarez1999@gmail.com>
2016-05-31 14:39:41 -0600

    intente hacer el 11

---
+++ prob_11.py
@@ -0,0 +1,9 @@
+def prob_11(palabra):
+	lista = list(palabra)
+	lista.split()
+	"".join(lista)
+	lista.reverse()
+	if palabra == lista:
+		print ("es palindromo")
+	else:
+		print ("no es palindromo")
