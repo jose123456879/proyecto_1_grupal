0c99bc72f56d7ab5fa42b29353981b2095d1f01d
jose123456879 <totitosst@gmail.com>
2016-06-01 08:32:02 -0600

    se realizo el problema_10

---
+++ prob_10.py
@@ -0,0 +1,9 @@
+def prob_10(a,b,c):
+	lista=[a, b, c]
+	tupla=tuple(lista)
+	if (tupla[0])==(tupla[1])==(tupla[2]):
+		return("Representa un tiangulo equilatero")
+	if(tupla[0]==(tupla[1])>(tupla[2]):
+		return("represeta un triangulo isoseles")
+	if (tupla[0])!=	(tupla[1])!=(tupla[2]):
+		return("Representa un triangulo escaleno")
