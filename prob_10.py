0e5a0ad7d5c28e3637a1850e0bd71337cd03fafc
jose123456879 <totitosst@gmail.com>
2016-06-01 08:35:45 -0600

    quickfix

--- prob_10.py
+++ prob_10.py
@@ -7,3 +7,5 @@ def prob_10(a,b,c):
 		return("represeta un triangulo isoseles")
 	if (tupla[0])!=	(tupla[1])!=(tupla[2]):
 		return("Representa un triangulo escaleno")
+	if(tupla[0])==0 or (tupla[1])==0 or (tupla[2])==0:
+		return("no es un triangulo.")
