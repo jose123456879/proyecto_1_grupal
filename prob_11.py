943918c266aa224884fa13e33e5430983a9617dc
ricardojuarez <ricardojuarez1999@gmail.com>
2016-06-03 14:29:02 -0600

    al fin termine el 11

--- prob_11.py
+++ prob_11.py
@@ -1,11 +1,8 @@
 def prob_11(pal):
 	palabra = pal.split()
-	pal.split()
-	j = "".join(palabra)
-	d = palabra.reverse()
-	pal2=pal.split()
-	s = "".join(pal2)
-	if d == s:
+	s = "".join(palabra)
+	var = s[::-1]
+	if s == var:
 		return ("es palindromo")
 	else:
 		return ("no es palindromo")
