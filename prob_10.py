316bc28573f75ab01837ce5b5268d9f93417f6af
jose123456879 <totitosst@gmail.com>
2016-06-10 08:21:42 -0600

    quickfix

--- prob_10.py
+++ prob_10.py
@@ -1,11 +1,11 @@
 def prob_10(a,b,c):
 	lista=[a, b, c]
 	tupla=tuple(lista)
-	if (tupla[0])==(tupla[1])==(tupla[2]):
+	if tupla[0]==tupla[1]==tupla[2]:
 		return("Representa un tiangulo equilatero")
-	if(tupla[0]==(tupla[1])>(tupla[2]):
+	if tupla[0]==tupla[1]>tupla[2]:
 		return("represeta un triangulo isoseles")
-	if (tupla[0])!=	(tupla[1])!=(tupla[2]):
+	if tupla[0]!=tupla[1]!=tupla[2]:
 		return("Representa un triangulo escaleno")
-	if(tupla[0])==0 or (tupla[1])==0 or (tupla[2])==0:
+	if tupla[0]==0 or tupla[1]==0 or tupla[2]==0:
 		return("no es un triangulo.")
